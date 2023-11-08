1d7b7e2d89f8522cf0d80bd1b19016e451a64a3d
HalimaTusSadiya <halimasaadiya66@gmail.com>
2023-11-08 18:35:59 +0600

    added service provider image in details page

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     
-    baseURL: "http://localhost:5000",
+    baseURL: "https://11-service-squade-server.vercel.app",
     withCredentials: true
 })
-//http://localhost:5000
+//https://11-service-squade-server.vercel.app
 const useAxios = () => {
     return  axiosInstance
 };
--- src/components/pages/ServiceDeatls/ServiceDetails/ServiceDetails.jsx
+++ src/components/pages/ServiceDeatls/ServiceDetails/ServiceDetails.jsx
@@ -9,24 +9,22 @@ import { AuthContext } from "../../../Provider/AuthProvider";
 import { dynamicTitle } from "../../../../utils/dynamicTitle";
 
 const ServiceDetails = () => {
-  dynamicTitle("details - ServiceSquad")
+  dynamicTitle("details - ServiceSquad");
   const [providerServices, setProviderServices] = useState([]);
-  const [loadedData, setLoadedData] = useState({})
-  const {user} = useContext(AuthContext)
-  const {id} = useParams()
-
+  const [loadedData, setLoadedData] = useState({});
+  const { user } = useContext(AuthContext);
+  const { id } = useParams();
 
   const axiosInstance = useAxios();
-  console.log({loggedInUser:user.email});
-  const loggedInUser =  user.email
-
+  console.log({ loggedInUser: user.email });
+  const loggedInUser = user.email;
 
   useEffect(() => {
     axiosInstance
       .get(`/api/v1/service/${id}`)
       .then((res) => {
-        setLoadedData(res.data)
-      console.log(res.data)
+        setLoadedData(res.data);
+        console.log(res.data);
       })
       .catch((err) => console.log(err.message));
   }, [id]);
@@ -42,28 +40,38 @@ const ServiceDetails = () => {
         setProviderServices(remainingServices);
       })
       .catch((err) => console.log(err.message));
-  }, [loadedData,id]);
+  }, [loadedData, id]);
 
   return (
     <div className="max-w-7xl mx-auto">
-      <div className="mb-10">
-        <SectionHeading text={`${loadedData?.serviceName || "" }`} />
-       { <DetailsCard service={loadedData} />}
+      <div className="grid w-full justify-center">
+        <div className="avatar mx-auto py-0 mt-5">
+          <div className="w-24 rounded-full">
+            <img src={user?.photoURL} />
+          </div>
+        </div>
+      </div>
+      <div className="mb-10 -mt-5">
+        <SectionHeading text={`${loadedData?.serviceName || ""}`} />
+        <h1 className="text-center -mt-10 text-2xl mb-10 md:text-4xl">
+          {loadedData?.serviceArea || ""}
+        </h1>
+        {<DetailsCard service={loadedData} />}
       </div>
 
       <div className="mb-10">
-        {providerServices.length 
-        ? 
-        <div>
-        <SectionHeading text={`${loadedData.providerName}'s Services`} />
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {providerServices?.map((service) => (
-            <UserServiceCard key={service._id} service={service} />
-          ))}
-        </div>
-      </div>
-        : ""}
-     
+        {providerServices.length ? (
+          <div>
+            <SectionHeading text={`${loadedData.providerName}'s Services`} />
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+              {providerServices?.map((service) => (
+                <UserServiceCard key={service._id} service={service} />
+              ))}
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
